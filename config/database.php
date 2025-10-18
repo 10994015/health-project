7666df21d9992eb9521dde31b5b013e49ab88b06
10994015 <a0938599191@gmail.com>
2025-10-18 18:45:55 +0800

    新增'scheme' => 'tls',

--- config/database.php
+++ config/database.php
@@ -135,6 +135,7 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
+            'scheme' => 'tls', // 強制使用 TLS 連線
         ],
 
         'cache' => [
